e63097fbd8084d487ef2eccdf845f59355b97bcf
Steven Cao <caohc2017@gmail.com>
2021-06-16 22:49:33 -0400

    Working 1.17 version

--- src/main/java/io/github/ultimateboomer/nofade/mixin/SplashScreenMixin.java
+++ src/main/java/io/github/ultimateboomer/nofade/mixin/SplashScreenMixin.java
@@ -2,7 +2,7 @@ package io.github.ultimateboomer.nofade.mixin;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawableHelper;
-import net.minecraft.client.gui.screen.SplashScreen;
+import net.minecraft.client.gui.screen.SplashOverlay;
 import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,15 +11,15 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(SplashScreen.class)
+@Mixin(SplashOverlay.class)
 public abstract class SplashScreenMixin extends DrawableHelper {
-    @Shadow private long applyCompleteTime;
+    @Shadow private long reloadCompleteTime;
 
     @Shadow @Final private MinecraftClient client;
 
     @Inject(method = "render", at = @At("HEAD"))
     private void onRender(MatrixStack matrices, int mouseX, int mouseY, float delta, CallbackInfo ci) {
-        if (this.applyCompleteTime > 1) {
+        if (this.reloadCompleteTime > 1) {
             this.client.setOverlay(null);
         }
     }
